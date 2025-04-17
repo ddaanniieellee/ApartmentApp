ac69c4ce196ce2ee13f6976cad15ec3788f32da3
JohnnyPer <jonathanjpersaud@gmail.com>
2025-04-16 20:36:00 -0400

    new change

--- App/models/user.py
+++ App/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=True, unique=True)
     phone = db.Column(db.String(20), nullable=False, unique=True)
     role = db.Column(db.String(10), nullable=False)  # 'tenant' or 'landlord'
 
