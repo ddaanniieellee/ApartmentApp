c4f26d173bfcc14d48f5fc88188177258793b99c
JohnnyPer <jonathanjpersaud@gmail.com>
2025-04-16 20:50:15 -0400

    Updated models

--- App/models/user.py
+++ App/models/user.py
@@ -6,9 +6,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True, default='')
-    phone = db.Column(db.String(20), nullable=False, unique=True, default='')
-    role = db.Column(db.String(10), nullable=False, default='')  # 'tenant' or 'landlord'
+    email = db.Column(db.String(120), nullable=True, unique=True)
+    phone = db.Column(db.String(20), nullable=True, unique=True)
+    role = db.Column(db.String(10), nullable=True)  # 'tenant' or 'landlord'
 
 
     def __init__(self, username, password):
