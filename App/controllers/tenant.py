3eaabff192a31827a22e84f5357d6f83bbecc469
JohnnyPer <jonathanjpersaud@gmail.com>
2025-04-19 19:01:54 -0400

    included "email" in signup process

--- App/controllers/tenant.py
+++ App/controllers/tenant.py
@@ -2,8 +2,8 @@ from App.models import Tenant
 from App.models import User
 from App.models import db
 
-def create_tenant(username,password):
-    newuser= Tenant(username=username,password=password)
+def create_tenant(username,email,password):
+    newuser= Tenant(username=username,email=email,password=password)
     db.session.add(newuser)
     db.session.commit()
     return newuser
