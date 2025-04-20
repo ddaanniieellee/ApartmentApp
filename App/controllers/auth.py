276e42b810e616d48d833cd397bb8b9cdabe41cf
JohnnyPer <jonathanjpersaud@gmail.com>
2025-04-19 21:52:51 -0400

    Only shows the "Add Property" tab if logged in as a Landlord

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -29,16 +29,26 @@ def setup_jwt(app):
 
 
 # Context processor to make 'is_authenticated' available to all templates
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from App.models import User
+
 def add_auth_context(app):
-  @app.context_processor
-  def inject_user():
-      try:
-          verify_jwt_in_request()
-          user_id = get_jwt_identity()
-          current_user = User.query.get(user_id)
-          is_authenticated = True
-      except Exception as e:
-          print(e)
-          is_authenticated = False
-          current_user = None
-      return dict(is_authenticated=is_authenticated, current_user=current_user)
+    @app.context_processor
+    def inject_user():
+        try:
+            verify_jwt_in_request()
+            user_id = get_jwt_identity()
+            current_user = User.query.get(user_id)
+            is_authenticated = current_user is not None
+            is_landlord = current_user.role == 'landlord' if current_user else False
+        except Exception as e:
+            print(e)
+            current_user = None
+            is_authenticated = False
+            is_landlord = False
+
+        return dict(
+            is_authenticated=is_authenticated,
+            current_user=current_user,
+            is_landlord=is_landlord
+        )
--- App/views/index.py
+++ App/views/index.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from flask_jwt_extended import get_jwt_identity
 from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
+
     properties = [
         {
             'id': 1,
